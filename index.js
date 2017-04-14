17f6070306f608987149e1d301773823afaaf9dc
longshuicy <chenwang.carrie@gmail.com>
2017-04-13 21:14:46 -0500

    cross origin

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var graphQLHTTP = require('express-graphql');
 var schema = require('./data/schema/schema.js');
+const cors = require('cors');
 
 //console.log(schema);
 
 const app = express();
 
-app.use(graphQLHTTP({
+app.use('/graphql',cors(), graphQLHTTP({
 	schema,
 	graphiql:true,	
 }))
